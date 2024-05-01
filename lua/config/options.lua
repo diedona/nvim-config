d4415a37119034533c5b0af5ecd9efe6215f6c24
diedona <diedona@gmail.com>
2024-05-01 13:57:00 -0300

    first status

---
+++ lua/config/options.lua
@@ -0,0 +1,22 @@
+local opt = vim.opt
+
+-- Tab / Indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+
+-- Search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+
+-- Behavior
+opt.mouse = "a"
+opt.clipboard:append("unnamedplus")
+
+opt.number = true
+opt.relativenumber = true
---
+++ lua/plugins/init.lua
@@ -0,0 +1,5 @@
+return {
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	"folke/which-key.nvim",
+	require "plugins.themes.github"
+}
