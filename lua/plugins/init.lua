d4db821d2122f5caca1d1e3968166250764b5ccf
diedona <diedona@gmail.com>
2024-05-06 10:52:10 -0300

    initial configuration with nvchad

---
+++ lua/plugins/init.lua
@@ -0,0 +1,68 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "pyright",
+        "mypy",
+        "black",
+        "gopls",
+        "gofumpt",
+        "goimports",
+        "golines",
+      },
+    },
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    ft = { "python" },
+    opts = function()
+      return require "configs.python-none-ls"
+    end,
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    ft = { "go" },
+    opts = function()
+      return require "configs.go-none-ls"
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "go"
+      },
+    },
+  },
+}
