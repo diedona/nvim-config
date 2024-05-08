a65f2a661a68b62f2453421585d158726cbb9578
diedona <diedona@gmail.com>
2024-05-07 22:05:13 -0300

    add diagnostics mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,3 +11,13 @@ map("i", "jk", "<ESC>")
 
 map("n", "<S-h>", ":bprev<CR>")
 map("n", "<S-l>", ":bnext<CR>")
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>do",
+  "<cmd>lua vim.diagnostic.open_float()<CR>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
