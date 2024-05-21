db11d2c36302dc0138309dd390f257e3a12e5c43
diedona <diedona@gmail.com>
2024-05-21 09:27:37 -0300

    conform configuration

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,3 +1,5 @@
+local conform = require "conform"
+
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
@@ -10,4 +12,12 @@ local options = {
   },
 }
 
-require("conform").setup(options)
+vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+  conform.format {
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 500,
+  }
+end, { desc = "Format file or range (in visual mode)" })
+
+conform.setup(options)
