9406ecb5fb879ab26c3e53597bafd4f81ff204ff
diedona <diedona@gmail.com>
2024-05-21 19:13:54 -0300

    insert leave

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = { "BufWritePre", "BufNewFile" }, -- uncomment for format on save
+    event = { "BufWritePre", "BufNewFile", "InsertLeave" }, -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
