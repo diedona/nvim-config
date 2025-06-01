8e776d4d361ba3fedcaeb76273edf30a60f59f7d
Diego Doná <diedona@gmail.com>
2025-06-01 12:16:53 -0300

    add lsp inicial

--- init.lua
+++ init.lua
@@ -1,48 +1,54 @@
-require 'core.options'
-require 'core.keymaps'
+require("core.options")
+require("core.keymaps")
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-	local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 
 	if vim.v.shell_error ~= 0 then
-		error('Error clonin lazy.nvim:\n' .. out)
+		error("Error clonin lazy.nvim:\n" .. out)
 	end
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
+require("lazy").setup({
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim"
+			"MunifTanjim/nui.nvim",
 		},
-		opts = {
-		},
-        config = function()
-            require('neo-tree').setup({
-            })
+		opts = {},
+		config = function()
+			require("neo-tree").setup({})
 
-            vim.keymap.set({'n', 'v'}, '<leader>e', ':Neotree toggle position=left<CR>', {noremap = true, silent = true})
-        end
-
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>e",
+				":Neotree toggle position=left<CR>",
+				{ noremap = true, silent = true }
+			)
+		end,
 	},
-	
-    require 'plugins.catppuccin',
 
-    require 'plugins.which',
+	require("plugins.catppuccin"),
 
-    require 'plugins.mini',
+	require("plugins.which"),
 
-    require 'plugins.bufferline',
+	require("plugins.mini"),
 
-    require 'plugins.telescope',
+	require("plugins.bufferline"),
 
-    require 'plugins.lazydev'
+	require("plugins.telescope"),
+
+	require("plugins.lazydev"),
+
+	require("plugins.lsp"),
+
+	require("plugins.conform"),
 })
