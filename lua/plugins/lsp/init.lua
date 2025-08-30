930bae111a09153332ba196d2a210da001dfe713
Diego Doná <diedona@gmail.com>
2025-08-30 20:47:21 -0300

    fix typo

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -168,7 +168,7 @@ return {
 		--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-		local server = require("plugins.lsp.servers")
+		local servers = require("plugins.lsp.servers")
 
 		-- Ensure the servers and tools above are installed
 		--
